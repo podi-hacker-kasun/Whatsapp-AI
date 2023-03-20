d11c2469d5712911155e6293c0d64114df5503c1
podi-hacker-kasun <123862013+podi-hacker-kasun@users.noreply.github.com>
2023-03-20 12:19:56 +0530

    Create KO.js

---
+++ Connect/KO.js
@@ -0,0 +1,18 @@
+{
+    "LANGUAGE": "Sinhala",
+    "LANGCODE": "SI",
+    "AUTHOR": "DarkWinzo",
+    "STRINGS": {
+     
+      "log": {
+         "CONNECT_MSG": "*Chat-AI 봇 다시 의 whatsapp 계저이 성공적 으로 여겨되 어스니다.* \n\n⚜️ 지윈 그룹에 가입⚜️\nhttps://chat.whatsapp.com/CZQwGCvcyNKIVW3gPF7GqX\n\n🔓️ 나 github 에 계 의겨을줘🔓️\nhttps://github.com/DarkWinzo/Whatsapp-AI\n\n🌐️ 이유로Youtube 채널 Subscribe 하다 🌐️\nhttps://www.youtube.com/@darkwinzo5483\n\n``` Whatsapp-AI 서비스를 서태해 주셔서 가사합니다.```\n\n"
+         },
+      "main": {
+          "REQUEST_API": "*🤖️ 잘못 :- \n*```죄11죄송합 니다0..Open-ai Key 만 나지 말 아주세요 Open AI key 하나 config.js 입 력`` \n\n_ 이 웹사이트에서 open ai key 만들다_ : https://beta .openai.com/account/api-keys",
+          "EXAMPLE": "보기",
+          "ERROR": "❌️오류가 발견되었습니다     .",
+          "ERROR_CODE":잘못된 필드"",
+          "ENTER_TEXT":일련의 단어를 입력하세요."```.```"
+      }
+    }
+}
